e5f50f7cefef418dc811c1236aabffe3d8ef7358
vasile.cristian <vasile.cristian@gmail.com>
2013-06-14 18:51:47 +0000

    - added the base code.

---
+++ src/RTTI.cpp
@@ -0,0 +1,41 @@
+/************************************************************************/
+/*File created on 09.2012 by Cristian Vasile (vasile.cristian@gmail.com)*/
+/************************************************************************/
+
+#include "vsgePCH.h"
+#include "RTTI.h"
+
+namespace vsge
+{
+    int RTTI::s_unicIDIncrement = 0;
+
+    RTTI::RTTI()
+    {
+    }
+    RTTI::~RTTI()
+    {
+        m_inheritanceChain.clear();
+    }
+
+    void* RTTI::IsKindOf(int type)                          
+    {   
+        return m_inheritanceChain[type];
+    }
+
+    void RTTI::PrintInheritanceChain()                          
+    {   
+        printf("=== ================== ===\n");
+
+        std::map<int, void*>::iterator inheritanceChainIT;
+        for ( inheritanceChainIT=m_inheritanceChain.begin() ; inheritanceChainIT != m_inheritanceChain.end(); inheritanceChainIT++ )
+            printf("'%d' = %d = %d\n", (*inheritanceChainIT).first, (*inheritanceChainIT).second, (*inheritanceChainIT).first);
+    }
+
+    const std::map<int, void*> RTTI::GetInheritanceChain()
+    {
+        return m_inheritanceChain;
+    }
+
+
+} //namespace vsge
+
