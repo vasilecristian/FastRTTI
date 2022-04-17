fbf8d21d97dc0ac4d80e14dac45efeb896bd301e
Cristian Vasile <vasile.cristian@gmail.com>
2022-04-17 14:54:51 +0300

    Added project for vs2019

--- tests/UsageTest/UsageTest.cpp
+++ tests/UsageTest/UsageTest.cpp
@@ -29,47 +29,47 @@ class AB_CD_E : public virtual AB, public virtual CD, public virtual E, public I
 int main(int argc, char* argv[])
 {
     AB_CD_E* obj1 = new AB_CD_E();
-    cout << "The inheritance chain (only in debug) \nfor AB_CD_E* obj1 = new AB_CD_E() object is: " << endl;
+    cout << "The inheritance chain list (available only in debug) \nfor AB_CD_E* obj1 = new AB_CD_E() object is: " << endl;
     for (auto& name : obj1->GetInheritanceChainNames())
     {
         cout << name << endl;
     }
-    cout << "Is object of A type? " << obj1->IsKindOf(IRTTI<A>::GetTypeID()) << endl;
-    cout << "Is object of E type? " << obj1->IsKindOf(IRTTI<E>::GetTypeID()) << endl;
+    cout << "Is object of A type? " << (obj1->IsKindOf(IRTTI<A>::GetTypeID()) ? "Yes" : "No") << endl;
+    cout << "Is object of E type? " << (obj1->IsKindOf(IRTTI<E>::GetTypeID()) ? "Yes" : "No") << endl;
     delete obj1;
     cout << endl;
 
     A* obj2 = new AB_CD_E();
-    cout << "The inheritance chain (only in debug) \nfor A* obj2 = new AB_CD_E() object is: " << endl;
+    cout << "The inheritance chain list (available only in debug) \nfor A* obj2 = new AB_CD_E() object is: " << endl;
     for (auto& name : obj2->GetInheritanceChainNames())
     {
         cout << name << endl;
     }
-    cout << "Is object of A type? " << obj2->IsKindOf(IRTTI<A>::GetTypeID()) << endl;
-    cout << "Is object of E type? " << obj2->IsKindOf(IRTTI<E>::GetTypeID()) << endl;
+    cout << "Is object of A type? " << (obj2->IsKindOf(IRTTI<A>::GetTypeID()) ? "Yes" : "No") << endl;
+    cout << "Is object of E type? " << (obj2->IsKindOf(IRTTI<E>::GetTypeID()) ? "Yes" : "No") << endl;
     delete obj2;
     cout << endl;
 
 
     AB* obj3 = new AB();
-    cout << "The inheritance chain (only in debug) \nfor AB* obj3 = new AB() object is : " << endl;
+    cout << "The inheritance chain list (available only in debug) \nfor AB* obj3 = new AB() object is : " << endl;
     for (auto& name : obj3->GetInheritanceChainNames())
     {
         cout << name << endl;
     }
-    cout << "Is object of A type? " << obj3->IsKindOf(IRTTI<A>::GetTypeID()) << endl;
-    cout << "Is object of E type? " << obj3->IsKindOf(IRTTI<E>::GetTypeID()) << endl;
+    cout << "Is object of A type? " << (obj3->IsKindOf(IRTTI<A>::GetTypeID()) ? "Yes" : "No") << endl;
+    cout << "Is object of E type? " << (obj3->IsKindOf(IRTTI<E>::GetTypeID()) ? "Yes" : "No") << endl;
     delete obj3;
     cout << endl;
 
     CD* obj4 = new CD();
-    cout << "The inheritance chain (only in debug) \nfor CD* obj4 = new CD() object is: " << endl;
+    cout << "The inheritance chain list (available only in debug) \nfor CD* obj4 = new CD() object is: " << endl;
     for (auto& name : obj4->GetInheritanceChainNames())
     {
         cout << name << endl;
     }
-    cout << "Is object of A type? " << obj4->IsKindOf(IRTTI<A>::GetTypeID()) << endl;
-    cout << "Is object of E type? " << obj4->IsKindOf(IRTTI<E>::GetTypeID()) << endl;
+    cout << "Is object of A type? " << (obj4->IsKindOf(IRTTI<A>::GetTypeID()) ? "Yes" : "No") << endl;
+    cout << "Is object of E type? " << (obj4->IsKindOf(IRTTI<E>::GetTypeID()) ? "Yes" : "No") << endl;
     delete obj4;
     cout << endl;
 
